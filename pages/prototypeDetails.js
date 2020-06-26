0290a90b67addad62824c8b32ff27f8afb96da13
Bryce Watson <brycewatson315@gmail.com>
2020-06-26 12:15:20 -0400

    added issue details page

--- pages/prototypeDetails.js
+++ pages/prototypeDetails.js
@@ -10,6 +10,23 @@ import HomeIcon from '@material-ui/icons/Home';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import ErrorIcon from '@material-ui/icons/Error';
 import DescriptionIcon from '@material-ui/icons/Description';
+import SmsIcon from '@material-ui/icons/Sms';
+import ModeCommentIcon from '@material-ui/icons/ModeComment';
+import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
+import FlightLandIcon from '@material-ui/icons/FlightLand';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import DoneIcon from '@material-ui/icons/Done';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Divider from '@material-ui/core/Divider';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import ChangeHistoryIcon from '@material-ui/icons/ChangeHistory';
 
 const useStyles = makeStyles((theme) => ({
     link: {
@@ -34,7 +51,10 @@ function PrototypeDetails(props) {
         ssr: false
       });
     const classes = useStyles();
-
+    const [open, setOpen] = React.useState(true);
+    const handleClick = () => {
+      setOpen(!open);
+    };
 
     return <Layout>
         <br/>
@@ -61,7 +81,88 @@ function PrototypeDetails(props) {
             {loading ? <Skeleton /> : 'Details (demo)'}
         </Typography>
         <br/>
-        <DynamicComponentWithNoSSR/>
+          <List 
+          component="nav" 
+          aria-label="company links"
+          subheader={
+            <ListSubheader component="div" id="company-list-subheader">
+              Issue Details
+            </ListSubheader>
+          }
+          >
+            <ListItem>
+              <ListItemIcon>
+                <SmsIcon />
+              </ListItemIcon>
+              <ListItemText primary="System Name" secondary='System 1' />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <DonutLargeIcon />
+              </ListItemIcon>
+              <ListItemText primary="System Status" secondary='Available'  />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <ModeCommentIcon />
+              </ListItemIcon>
+              <ListItemText primary="Issue Name" secondary='Issue 1'  />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <EqualizerIcon />
+              </ListItemIcon>
+              <ListItemText primary="Issue Status" secondary='Scheduled for maintenance' />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <FlightTakeoffIcon />
+              </ListItemIcon>
+              <ListItemText primary="Start Time" secondary='6/26/2020 12:00 PM EST' />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <FlightLandIcon />
+              </ListItemIcon>
+              <ListItemText primary="Expected Resolution"  secondary='6/26/2020 6:00 PM EST'/>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <DoneIcon />
+              </ListItemIcon>
+              <ListItemText primary="Resolution" secondary='Pending maintenance completion'/>
+            </ListItem>
+            <ListItem button onClick={handleClick}>
+              <ListItemIcon>
+                <DescriptionIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Description" secondary="1 update posted 37 min ago" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItem className={classes.nested}>
+                  <ListItemIcon>
+                    <ChangeHistoryIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Preparing to take system offline for maintenance" secondary='6/26/2020 9:16 PM EST' />
+                </ListItem>
+                <ListItem className={classes.nested}>
+                  <ListItemIcon>
+                    <ChangeHistoryIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Issue moved to priority maintenance queue" secondary='6/25/2020 5:27 PM EST' />
+                </ListItem>
+                <ListItem className={classes.nested}>
+                  <ListItemIcon>
+                    <ChangeHistoryIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Issue logged" secondary='6/25/2020 4:13 PM EST'/>
+                </ListItem>
+              </List>
+            </Collapse>
+          </List>
+          <Divider />
     </Layout>
 }
 
