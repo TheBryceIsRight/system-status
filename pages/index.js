7c1904f21dc03facccd0026014bbaf7fc6392127
Bryce Watson <brycewatson315@gmail.com>
2020-07-02 17:47:14 -0400

    removed redux again

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import clsx from 'clsx';
 
 import React, { useState }  from 'react'
 import render from 'react-dom'
-import { createMuiTheme, fade,makeStyles, ThemeProvider, withStyles, responsiveFontSizes } from '@material-ui/core/styles'
+import { createMuiTheme, fade, makeStyles, ThemeProvider, withStyles, responsiveFontSizes } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Toolbar from '@material-ui/core/Toolbar'
