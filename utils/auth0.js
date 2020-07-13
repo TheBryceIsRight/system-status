615cd7b58c40ccc5765725559950fb6ab4b9cd34
Bryce Watson <brycewatson315@gmail.com>
2020-07-13 18:00:20 -0400

    more revision of Auth0 rules to run in prod

--- utils/auth0.js
+++ utils/auth0.js
@@ -5,7 +5,7 @@ export default initAuth0({
   clientId: 'a5gj4Gifg1CAc8HvHhjD9EBa7c2p77jD',
   clientSecret: 'xOiyqYaveMYLm6AoIOGR8U4AiGbOIsmht-K0EGd_88IwjkvI8RITXNNRUbKuc2bh',
   scope: 'openid profile',
-  redirectUri: 'https://system-status.vercel.app//api/callback',
+  redirectUri: 'https://system-status.vercel.app/api/callback',
   postLogoutRedirectUri: 'https://system-status.vercel.app/',
   session: {
     // The secret used to encrypt the cookie.
